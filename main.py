bdc4c04f4be3f63c21ab53c38f8d916167c871be
Imran Remtulla <contact@imranr.dev>
2025-09-01 15:03:59 -0400

    Increase timeout

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
         fields[0].click()
     if (email and password): # If not defined, you can login manually
         driver.find_elements(By.CSS_SELECTOR, "div > div > div > button").pop().click()
-    WebDriverWait(driver, 3600).until(EC.url_changes(driver.current_url)) # Long timeout needed for manual login or 2FA
+    WebDriverWait(driver, 18000).until(EC.url_changes(driver.current_url)) # Long timeout needed for manual login or 2FA
     driver.get("https://my.wealthsimple.com/app/activity")
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button/div/div/div[2]/p[1]")))
     time.sleep(2) # If you need to scroll down to 'Load More', increase this timeout to have enough time to scroll manually (scrolling is not automated)
